c75b963d40b20e251dfc9199b77841b969dd1678
Hachemi Ibrahim <123593839+HachemiIbrahim@users.noreply.github.com>
2023-06-05 11:12:24 +0100

    the user logout

--- app/src/main/java/com/example/chatapp/MainActivity.java
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -1,17 +1,23 @@
 package com.example.chatapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     private ViewPager mViewPager;
     private TabLayout mTabLayout;
     private TabsAccessorAdapter tabsAccessorAdapter;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +30,33 @@ public class MainActivity extends AppCompatActivity {
         tabsAccessorAdapter = new TabsAccessorAdapter(getSupportFragmentManager());
         mViewPager.setAdapter(tabsAccessorAdapter);
 
+        auth = FirebaseAuth.getInstance();
+
         mTabLayout = findViewById(R.id.TabLayout);
         mTabLayout.setupWithViewPager(mViewPager);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_options , menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.logout){
+            auth.signOut();
+            startActivity(new Intent(MainActivity.this , LoginActivity.class));
+        }
+
+        if(item.getItemId() == R.id.find_friends){
+
+        }
+
+        if (item.getItemId() == R.id.settings){
+
+        }
+        return true;
     }
 }
