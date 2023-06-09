21411c69859557bd3a53bf418bb016961981c5a7
Hachemi Ibrahim <123593839+HachemiIbrahim@users.noreply.github.com>
2023-06-10 00:38:20 +0100

    displaying group messages

--- app/src/main/java/com/example/chatapp/GroupChatActivity.java
+++ app/src/main/java/com/example/chatapp/GroupChatActivity.java
@@ -1,18 +1,23 @@
 package com.example.chatapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -23,13 +28,14 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.HashMap;
+import java.util.Iterator;
 
 public class GroupChatActivity extends AppCompatActivity {
 
     private EditText Message;
     private ImageView Send;
     private ScrollView scrollView;
-    private TextView displayMessages;
+    private LinearLayout chatContainer;
     private String currentGroupName,userId,userName,currentDate,currentTime;
     private FirebaseAuth auth;
     private DatabaseReference reference;
@@ -47,8 +53,7 @@ public class GroupChatActivity extends AppCompatActivity {
         Message = findViewById(R.id.message);
         Send = findViewById(R.id.send_message);
         scrollView = findViewById(R.id.scroll_view);
-        displayMessages = findViewById(R.id.group_chat_text_display);
-
+        chatContainer = findViewById(R.id.chatContainer);
         auth = FirebaseAuth.getInstance();
         reference = FirebaseDatabase.getInstance().getReference();
         userId = auth.getCurrentUser().getUid();
@@ -72,6 +77,72 @@ public class GroupChatActivity extends AppCompatActivity {
                 Message.setText("");
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        reference.child("Groups").child(currentGroupName).addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                if(snapshot.exists()){
+                    displayAllMessages(snapshot);
+                }
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                if(snapshot.exists()){
+                    displayAllMessages(snapshot);
+                }
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void displayAllMessages(DataSnapshot snapshot) {
+        Iterator iterator = snapshot.getChildren().iterator();
+        while (iterator.hasNext()){
+            String Date = ((DataSnapshot) iterator.next()).getValue().toString();
+            String Message = ((DataSnapshot) iterator.next()).getValue().toString();
+            String Name = ((DataSnapshot) iterator.next()).getValue().toString();
+            String Time = ((DataSnapshot) iterator.next()).getValue().toString();
+
+            TextView textView = new TextView(this);
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+            );
+            layoutParams.setMargins(16,8,16,8);
+            textView.setPadding(16,8,16,8);
+            textView.setTextColor(Color.BLACK);
+            if(Name.equals(userName)){
+                textView.setBackgroundResource(R.drawable.outgoing_message_background);
+                layoutParams.gravity = Gravity.END;
+            }else{
+                textView.setBackgroundResource(R.drawable.incoming_message_background);
+                layoutParams.gravity = Gravity.START;
+            }
+
+            textView.setLayoutParams(layoutParams);
+            textView.setText(Name + " : \n" + Message);
+
+            chatContainer.addView(textView);
+        }
     }
 
     private void SendMessages() {
