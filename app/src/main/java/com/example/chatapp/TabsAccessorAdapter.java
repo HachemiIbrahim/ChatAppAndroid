aba726930e1189bf933efb58aa29e2f672d7cfb1
Hachemi Ibrahim <123593839+HachemiIbrahim@users.noreply.github.com>
2023-06-16 17:46:35 +0100

    request fragment

--- app/src/main/java/com/example/chatapp/TabsAccessorAdapter.java
+++ app/src/main/java/com/example/chatapp/TabsAccessorAdapter.java
@@ -26,6 +26,9 @@ public class TabsAccessorAdapter extends FragmentPagerAdapter {
             case 2 :
                 ContactFragment contactFragment = new ContactFragment();
                 return contactFragment;
+            case 3 :
+                RequestFragment requestFragment = new RequestFragment();
+                return requestFragment;
             default:
                 return null;
         }
@@ -33,7 +36,7 @@ public class TabsAccessorAdapter extends FragmentPagerAdapter {
 
     @Override
     public int getCount() {
-        return 3;
+        return 4;
     }
 
     @Nullable
@@ -46,6 +49,8 @@ public class TabsAccessorAdapter extends FragmentPagerAdapter {
                 return "Groups";
             case 2 :
                 return "Contacts";
+            case 3 :
+                return "Requests";
             default:
                 return null;
         }
